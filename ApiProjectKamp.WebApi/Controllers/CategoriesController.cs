afb27dea854dd6bf370894a5c6ea885200abbc6c
Çağrı Özay <cagriozay13@gmail.com>
2026-01-04 22:05:49 +0300

    first commit

---
+++ ApiProjectKamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+using ApiProjectKamp.WebApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiProjectKamp.WebApi.Entities;
+
+namespace ApiProjectKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemib başarılı");
+        }
+    }
+}
---
+++ ApiProjectKamp.WebApi/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace ApiProjectKamp.WebApi.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
